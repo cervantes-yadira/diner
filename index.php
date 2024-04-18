424d90e3353d405a3a0cade1f9eb1856f16be742
Yadira Cervantes <cervantes.yadira@student.greenriver.edu>
2024-04-18 03:04:18 -0400

    Add order pages and routes

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $f3->route('GET /menus/breakfast', function() {
 
 });
 
-// breakfast route
+// lunch route
 $f3->route('GET /menus/lunch', function() {
     // echo '<h1>My Breakfast Menu</h1>';
     // render a view page
@@ -38,12 +38,41 @@ $f3->route('GET /menus/lunch', function() {
 
 });
 
-// breakfast route
+// dinner route
 $f3->route('GET /menus/dinner', function() {
     // echo '<h1>My Breakfast Menu</h1>';
     // render a view page
     $view = new Template();
     echo $view->render('views/dinner-menu.html');
+
+});
+
+// order1 route
+$f3->route('GET|POST /order1', function($f3) {
+    // echo '<h1>My Breakfast Menu</h1>';
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+//        echo "<p>POST</p>";
+//        var_dump($_POST);
+
+        // get data from POST array
+        $food =$_POST['food'];
+        $meal =$_POST['meal'];
+
+        $f3->set('SESSION.food', $food);
+        $f3->set('SESSION.meal', $meal);
+    }
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+
+});
+
+// order2 route
+$f3->route('GET /order2', function() {
+    // echo '<h1>My Breakfast Menu</h1>';
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
 
 });
 
